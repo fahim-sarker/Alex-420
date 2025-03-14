c79e588f3a2eee1beb0a2545fd9f1d267e96a549
fahim-sarker <sarkerfahim599@gmail.com>
2025-03-14 21:11:06 +0600

    update slice

--- src/Components/Slice/Productslice.js
+++ src/Components/Slice/Productslice.js
@@ -13,14 +13,26 @@ export const Produtcslice = createSlice({
         state.baritem.push(action.payload)
         localStorage.setItem("cartProduct", JSON.stringify(state.baritem))
       } 
-      // else {
-      //   state.baritem[newItemIndex].qty += 1
-      //   localStorage.setItem("cartProduct", JSON.stringify(state.baritem))
-      // }
+      else {
+        state.baritem[newItemIndex].qty += 1
+        localStorage.setItem("cartProduct", JSON.stringify(state.baritem))
+      }
+    },
+    barreceipt : (state, action) => {
+      const newItemIndex = state.baritem.findIndex((item) => item.id == action.payload.id);
+
+      if (newItemIndex != -1) {
+        state.baritem.push(action.payload)
+        localStorage.setItem("cartProduct", JSON.stringify(state.baritem))
+      } 
+      else {
+        state.baritem[newItemIndex].qty += 1
+        localStorage.setItem("cartProduct", JSON.stringify(state.baritem))
+      }
     }
   }
 })
 
-export const { barcartitem } = Produtcslice.actions
+export const { barcartitem,barreceipt } = Produtcslice.actions
 
 export default Produtcslice.reducer
